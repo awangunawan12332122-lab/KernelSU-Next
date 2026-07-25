48c0310e2d05c1d8aa25e0d35c380c80d547149c
awangunawan12332122-lab <awangunawan12332122@gmail.com>
2026-07-25 18:45:01 +0700

    Update setup.sh

--- kernel/setup.sh
+++ kernel/setup.sh
@@ -2,8 +2,8 @@
 set -eu
 
 GKI_ROOT=$(pwd)
-OWNER="KernelSU-Next"
-REPO="$OWNER"
+OWNER="awangunawan12332122-lab"
+REPO="KernelSU-Next"
 
 display_usage() {
     echo "Usage: $0 [--cleanup | <commit-or-tag>]"
